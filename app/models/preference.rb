5ab5ef6db3cb2f1531063d7313e206229c399cdd
mstmari <michael.landrum.st.marie@gmail.com>
2018-06-12 14:17:37 -0400

    Done.

---
+++ app/models/preference.rb
@@ -0,0 +1,24 @@
+class Preference < ActiveRecord::Base
+
+
+  # def Preference::artist_sort_order
+  #   Artist.order(name: :desc)
+  #
+  # end
+  #
+  # def Preference::song_sort_order
+  #   Song.order(name: :desc)
+  # end
+  #
+  # def Preference::allow_create_artists
+  #   if false
+  #     redirect_to artists_path
+  #   end
+  # end
+  #
+  # def Preference::allow_create_songs
+  #   if false
+  #     redirect_to songs_path
+  #   end
+  # end
+end
